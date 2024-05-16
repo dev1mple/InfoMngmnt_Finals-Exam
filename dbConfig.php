9413d33f444f8d39859ec6d099840a147c6d4bd7
Jerrel Dave Treceñe <167535920+dev1mple@users.noreply.github.com>
2024-05-16 20:59:42 +0800

    Add files via upload

---
+++ dbConfig.php
@@ -0,0 +1,14 @@
+<?php 
+    // Database connection details
+    $host = "localhost"; // Hostname
+    $user = "root"; // Username
+    $password = ""; // Password
+    $dbname = "accounts"; // Database name (should be the correct database name)
+    $dsn = "mysql:host={$host};dbname={$dbname}"; // Data Source Name (DSN) for PDO
+
+    // Create a new PDO instance for database connection
+    $conn = new PDO($dsn, $user, $password);
+
+    // Set the time zone for the database connection (GMT+8 for Philippines Standard Time)
+    $conn->exec("SET time_zone = '+08:00';");
+?>
